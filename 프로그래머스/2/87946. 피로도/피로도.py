db7dc8a5a1f5107d9ebef67075b243474c1198e0
apricity2u <gkdms9849@gmail.com>
2024-12-03 21:41:39 +0900

    [level 2] Title: 피로도, Time: 36.70 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+
+def solution(k, dungeons):
+    answer = -1
+
+    for each_cases in permutations(dungeons):
+        cnt = 0
+        tmp_k = k
+        for dungeon in each_cases:
+            if tmp_k >= dungeon[0]:
+                tmp_k -= dungeon[1]
+                cnt += 1
+            else:
+                break
+        answer = max(answer, cnt)
+
+    return answer
