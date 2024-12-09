5b89bd42ca02ab7bba73b7ae9e8b2f1cfe255a12
apricity2u <gkdms9849@gmail.com>
2024-12-10 01:55:27 +0900

    [Silver II] Title: 최소 힙, Time: 112 ms, Memory: 39124 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,23 @@
+from heapq import heappop, heappush, heapify
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+nums = []
+for _ in range(N):
+    nums.append(int(input()))
+
+heap = []
+
+for num in nums:
+    if num > 0 :
+        heappush(heap, num)
+    elif num == 0 :
+
+        if len(heap) == 0 :
+            print(0)
+            continue
+
+        print(heappop(heap))
