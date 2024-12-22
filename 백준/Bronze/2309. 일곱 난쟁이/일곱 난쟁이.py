60f08d21ed9ef150cb1cc09c35ac538e813c1df0
apricity2u <gkdms9849@gmail.com>
2024-12-22 18:09:39 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+import sys
+input = sys.stdin.readline
+
+dwarfs = [int(input()) for _ in range(9)]
+dwarfs.sort()
+
+for combi in combinations(dwarfs, 7):
+  if sum(combi) == 100:
+    for dwarf in combi:
+      print(dwarf)
+    break
