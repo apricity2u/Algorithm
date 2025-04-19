d061ea2f261f917ef826a841562aa2cff8f9f677
apricity2u <gkdms9849@gmail.com>
2025-04-20 00:06:09 +0900

    [Bronze II] Title: 하얀 칸, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+board = [list(input().rstrip()) for _ in range(8)]
+
+cnt = 0
+
+for i in range(8):
+    for j in range(8):
+
+        if i % 2 == j % 2 and board[i][j] == "F":
+            cnt += 1
+
+print(cnt)
